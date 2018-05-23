a4713406c573fb1ee2c53f630f03e7575037513d
wuyuler <971680807@qq.com>
2018-05-23 22:32:48 +0800

    将程序导入到了win10系统下

--- src/utils/DBhelper.java
+++ src/utils/DBhelper.java
@@ -11,7 +11,7 @@ import java.util.ArrayList;
 
 public class DBhelper {
 	private static final String driver = "org.postgresql.Driver";
-	private static final String url ="jdbc:postgresql://localhost:5432/exampledb";
+	private static final String url ="jdbc:postgresql://localhost:5432/example";
 	private static final String username = "postgres";
 	private static final String password = "admin";
 	private static Connection cnn = null;
@@ -38,18 +38,18 @@ public class DBhelper {
 		}
 		else return cnn;
 	}
-//// 测试数据库连接
-//	public static void main(String[] args) {
-//		try {
-//			Connection con = DBhelper.getConnection();
-//			if(cnn!=null) System.out.println("链接正常");
-//			else System.out.println("连接失败");
-//			
-//		} catch (Exception e) {
-//			// TODO: handle exception
-//			e.printStackTrace();
-//		}
-//	}
+// 测试数据库连接
+	public static void main(String[] args) {
+		try {
+			Connection con = DBhelper.getConnection();
+			if(cnn!=null) System.out.println("链接正常");
+			else System.out.println("连接失败");
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+	}
 //	
 	
 	
